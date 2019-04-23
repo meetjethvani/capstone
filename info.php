7b30a148618c42b1f9fba7beb5995c169d74135a
Meet Jethvani <mmj25294@gmail.com>
2019-04-23 12:09:38 -0400

    Update info.php

--- info.php
+++ info.php
@@ -21,7 +21,8 @@
 	if (!mysqli_query($dbconnect, $query)) {
         	die('An error occurred when submitting your review.');
     	} else {	
-		echo "Name: $Name <br> City: $City <br> <br> Thanks for your detail!";
+		echo "Name: $Name <br> City: $City <br> <br>"
+		echo "Thanks for your detail!";
     	}
 ?>
 </body>
